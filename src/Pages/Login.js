b2d5d993a0750b321ac2d3cc93ffd342566e1a2e
cheroshena <fwpcheroshena@gmail.com>
2024-04-23 22:05:47 +0530

    Done

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,10 +1,71 @@
 import React from 'react'
-
+import { Link } from "react-router-dom";
+import Loginart from "../assets/Loginart.png";
+import Google from "../assets/Google.png";
+import Facebook from "../assets/Facebook.png";
 
 function Login () {
   return (
-    <div>
-      Login
+    <div className="signin">
+      <div className="signin0">
+        <div className="signin1">
+          <div className="signin10">
+            <div className="signin11">
+              <div className="signin111">Login</div>
+              <div className="signin112">
+                Today is a new day. It's your day. You shape it. Sign in to
+                start managing your projects.
+              </div>
+              <div className="signin113">
+                Email
+                <input
+                  name="myInput"
+                  placeholder="Example@email.com"
+                  className="signinput"
+                />
+              </div>
+              <div className="signin113">
+                Password
+                <input
+                  name="myInput"
+                  placeholder="at least 8 characters"
+                  className="signinput"
+                />
+                <div className="signin114">Forget password?</div>
+              </div>
+
+              <div className="signin115">Sign in</div>
+            </div>
+            or
+            <div className="signin12">
+              <img src={Google} width="15%" className="logosignin" alt="aaa" />
+              SIgn in with google
+            </div>
+            <div className="signin13">
+              <img
+                src={Facebook}
+                width="15%"
+                className="logosignin"
+                alt="aaa"
+              />
+              signin with facebook
+            </div>
+            <div className="signin14">
+              Don't you have an account?{" "}
+              <Link
+                to="/signup"
+                className="sss"
+                style={{ textDecoration: "none" }}
+              >
+                Sign up
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="signin2">
+          <img src={Loginart} width="100%" height="100%" alt="aaa" />
+        </div>
+      </div>
     </div>
   )
 }
--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,10 +1,70 @@
 import React from 'react'
-
+import { Link } from "react-router-dom";
+import Loginart from "../assets/Loginart.png";
+import Google from "../assets/Google.png";
+import Facebook from "../assets/Facebook.png";
 
 function Register () {
   return (
-    <div>
-      Register
+    <div className="signin">
+      <div className="signin0">
+        <div className="signin1">
+          <div className="signin10">
+            <div className="signin11">
+              <div className="signin111">Sign Up / Register</div>
+              <div className="signin112">
+                Sign up today. Cover all your Favorite Places with us.
+              </div>
+              <div className="signin113">
+                Email
+                <input
+                  name="myInput"
+                  placeholder="Example@email.com"
+                  className="signinput"
+                />
+              </div>
+              <div className="signin113">
+                Password
+                <input
+                  name="myInput"
+                  placeholder="at least 8 characters"
+                  className="signinput"
+                />
+                <div className="signin114">Forget password?</div>
+              </div>
+
+              <div className="signin115">Sign up</div>
+            </div>
+            or
+            <div className="signin12">
+              <img src={Google} width="15%" className="logosignin" alt="aaa" />
+              SIgn in with google
+            </div>
+            <div className="signin13">
+              <img
+                src={Facebook}
+                width="15%"
+                className="logosignin"
+                alt="aaa"
+              />
+              signin with facebook
+            </div>
+            <div className="signin14">
+              you have an account?{" "}
+              <Link
+                to="/signin"
+                className="sss"
+                style={{ textDecoration: "none" }}
+              >
+                Sign in
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="signin2">
+          <img src={Loginart} width="100%" height="100%" alt="aaa" />
+        </div>
+      </div>
     </div>
   )
 }
